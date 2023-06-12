85588976796d4d5f15f4c84be2e0f0241dc0875b
YamanAyoun <yamanayoun0@gmail.com>
2023-06-12 16:18:30 +0300

    Add project files.

---
+++ numbersGame/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace numbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSequence();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid input: " + ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Number is too large or too small: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something wrong: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program is complete.");
+            }
+        }
+
+        static void StartSequence()
+        {
+            Console.WriteLine("Welcome to my game! Let's do some math!");
+            Console.WriteLine("Please enter a number greater than zero");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbers = new int[size];
+            numbers = Populate(numbers);
+
+            int sum = GetSum(numbers);
+            int product = GetProduct(numbers, sum);
+            decimal quotient = GetQuotient(product);
+
+            Console.WriteLine($"Your array is size = {numbers.Length}");
+            
+            /*for (int i = 0; i <= numbers.Length; i++)
+            {
+                Console.WriteLine($"The numbers in the array are = {numbers[i]}");
+            };*/
+            Console.WriteLine($"The Sum of the array is: {sum}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Quotient: {quotient}");
+        }
+
+        static int[] Populate(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Please enter number: {i + 1} of {numbers.Length}");
+                string input = Console.ReadLine();
+                numbers[i] = Convert.ToInt32(input);
+   
+            }
+
+            return numbers;
+        }
+
+        static int GetSum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+
+            if (sum < 20)
+            {
+                throw new Exception($"Value of {sum} is too low");
+            }
+
+            return sum;
+        }
+
+        static int GetProduct(int[] numbers, int sum)
+        {
+            Console.WriteLine($"Select a random number between 1 and {numbers.Length}:");
+            int index = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (index < 0 || index >= numbers.Length)
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+
+            int product = sum * numbers[index];
+            return product;
+        }
+
+        static decimal GetQuotient(int product)
+        {
+            Console.WriteLine($"Enter a number to divide {product} by:");
+            decimal dividend = Convert.ToDecimal(Console.ReadLine());
+
+            if (dividend == 0)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                return 0;
+            }
+
+            decimal quotient = decimal.Divide(product, dividend);
+            return quotient;
+        }
+    }
+    }
